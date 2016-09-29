8f941b8dd13d44e3185cccf56550968f6c02c488
Mike Meadows <mmeadow33@gmail.com>
2016-09-28 22:02:51 -0500

    changing state to change views

--- js/Note.js
+++ js/Note.js
@@ -1,20 +1,45 @@
 var Note = React.createClass({
-  edit: function (){
-    alert("editing note")
-  },
-  remove: function (){
-    alert("removing note")
-  },
+    getInitialState: function() {
+        return {editing: false}
+    },
+    edit: function() {
+        this.setState({editing: true});
+    },
+    save: function() {
+        this.setState({editing: false});
+    },
+    remove: function() {
+        alert('removing note');
+    },
+    renderDisplay: function() {
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
+                <span>
+                    <button onClick={this.edit}
+                            className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button onClick={this.remove}
+                            className="btn btn-danger glyphicon glyphicon-trash"/>
+                </span>
+            </div>
+            );
+    },
+    renderForm: function() {
+        return (
+            <div className="note">
+            <textarea defaultValue={this.props.children}
+            className="form-control"></textarea>
+            <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+            </div>
+            )
+    },
     render: function() {
-      return (
-        <div className="note">
-        <p>{this.props.children}</p>
-          <span>
-            <button onClick={this.edit} className="mmm">Add</button>
-            <button onClick={this.remove}  className="msdsmm">Delete</button>
-          </span>
-        </div>
-      )
+        if (this.state.editing) {
+            return this.renderForm();
+        }
+        else {
+            return this.renderDisplay();
+        }
     }
 });
 
